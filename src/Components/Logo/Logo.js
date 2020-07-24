76c7ce9de4b5c00a027c01bdf884571c68fe02b1
WashOliveira <washingtonfawkes@gmail.com>
2020-07-24 01:03:32 -0300

    Adicionando componentes
    Adicionando componentes

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt className="tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+			 	<div className="Tilt-inner pa3">
+			 		<img style={{paddingTop: '20px'}} alt='logo' src={brain}/>
+			 	</div>
+			</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
